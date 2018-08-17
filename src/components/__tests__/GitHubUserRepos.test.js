c8310a4323514514277c73ed0a2eaf0243f5d337
AmlisPanda <37158534+AmlisPanda@users.noreply.github.com>
2018-08-17 16:36:05 -0400

    Update GitHubUserRepos.test.js

--- src/components/__tests__/GitHubUserRepos.test.js
+++ src/components/__tests__/GitHubUserRepos.test.js
@@ -13,12 +13,12 @@ describe('GitHubUserRepos', () => {
 
 		mockedEvent = { preventDefault: jest.fn() };
 
-	 	repoUser = [
-            {
-                id: 2,
-                updated_at: "2018-04-15T19:00:53Z"
-            }
-        ];
+		repoUser = [
+		    {
+			id: 2,
+			updated_at: "2018-04-15T19:00:53Z"
+		    }
+		];
 
 		wrapper = mount(<GitHubUserRepos />)
 	})
@@ -27,41 +27,41 @@ describe('GitHubUserRepos', () => {
 	it('renders component correctly with "top" = 10 if not defined', () => {
 
 		expect(wrapper.prop("top")).toBe(10);
-    });
+    	});
 
 
-    it('should change "sortOrder" state when sort order changes', () => {
+    	it('should change "sortOrder" state when sort order changes', () => {
 
-  		wrapper.find("#sortList").simulate("change", { target : { value: "1" } });
-	  	expect(wrapper.state("sortOrder")).toBe(1);
-    });
+		wrapper.find("#sortList").simulate("change", { target : { value: "1" } });
+		expect(wrapper.state("sortOrder")).toBe(1);
+    	});
 
 
-    it("verify that handleFetch isn't called when the username is null", () => {
+    	it("verify that handleFetch isn't called when the username is null", () => {
 
-    	const instance = wrapper.instance();
-    	const username = "";
+		const instance = wrapper.instance();
+		const username = "";
 
-    	instance.search = jest.fn();
-    	instance.handleFetch = jest.fn();
-    	instance.search(mockedEvent, username);
+		instance.search = jest.fn();
+		instance.handleFetch = jest.fn();
+		instance.search(mockedEvent, username);
 
-    	expect(instance.search).toHaveBeenCalled();
-    	expect(instance.handleFetch).toHaveBeenCalledTimes(0);
-    });
+		expect(instance.search).toHaveBeenCalled();
+		expect(instance.handleFetch).toHaveBeenCalledTimes(0);
+    	});
 
 
   	it("verify that handleFetch is called when the username is defined", () => {
 
-        const instance = wrapper.instance();
-        const username = "username";
+		const instance = wrapper.instance();
+		const username = "username";
 
-        instance.search = jest.fn();
-        instance.handleFetch = jest.fn().mockReturnValue(repoUser);
-        instance.search(mockedEvent, username);
+		instance.search = jest.fn();
+		instance.handleFetch = jest.fn().mockReturnValue(repoUser);
+		instance.search(mockedEvent, username);
 
-        expect(instance.search).toHaveBeenCalled();
-        expect(instance.search).toHaveBeenCalledWith(mockedEvent, username);
-        expect(instance.handleFetch()).toEqual(repoUser);
-    });
+		expect(instance.search).toHaveBeenCalled();
+		expect(instance.search).toHaveBeenCalledWith(mockedEvent, username);
+		expect(instance.handleFetch()).toEqual(repoUser);
+    	});
 });
